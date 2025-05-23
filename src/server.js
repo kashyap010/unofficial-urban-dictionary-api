8d58e4b0d9af8f025762e6a799058c503b9df790
kashyap010 <kashyapshirodkar@gmail.com>
2025-05-23 22:33:50 +0530

    enable cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const cors = require('cors');
 const setupRoutes = require("./routes");
+
+app.use(cors());
 
 app.use(express.static("public"));
 
